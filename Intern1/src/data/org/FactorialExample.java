cd3aa8af412032798acbad6ce82b6b645e1428a9
Rahul60602812 <55742414+Rahul60602812@users.noreply.github.com>
2019-09-25 16:40:02 +0530

    changing

--- Intern1/src/data/org/FactorialExample.java
+++ Intern1/src/data/org/FactorialExample.java
@@ -7,7 +7,7 @@ public class FactorialExample {
 		  for(i=1;i<=number;i++){    
 		      fact=fact*i;    
 		  }    
-		  System.out.println("Factorial of "+number+" is: "+fact);    
+		  System.out.println("Factorial of "+number+" is: "+fact);
 		 }
 
 	}
